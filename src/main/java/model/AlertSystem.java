2298ba7034099ce2f757d83ec6232217d055f67e
FatimaGuermach <fatimaguermach2003@gmail.com>
2025-01-05 14:21:08 +0100

    delete file

--- src/main/java/model/AlertSystem.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class AlertSystem {
-    public void checkForIntrusion(String packetInfo) {
-        if (packetInfo.contains("DoS")) {
-            System.out.println("Alerte : attaque DoS détectée !");
-        }
-    }
-}
